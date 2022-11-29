7f21ba0db24349c39fc11f153ff7c2a77b647bcb
SlipsKnuten <gurra.bergqvist@gmail.com>
2022-11-29 01:15:38 +0100

    Valid_anagram

--- Valid_anagram.js
+++ Valid_anagram.js
@@ -1,8 +1,21 @@
-var isAnagram = function(s, t) {
-    if (s.includes("anagram") || t.includes("anagram")) {
-        return true;
+
+    var isAnagram = function(s,t){
+        if (s.length !== t.length) return false
+        const countS = {}
+        const countT = {}
+        const N = s.length
+
+        for(let i = 0; i < N; i++){
+            if(!countS[s[i]]) countS[s[i]]=0 //countS är ett objekt och måste sättas till 0 så det blir en INT om vi inte har sätt karaktären ännu. om karatären inte förekommer i strängen så sätter vi värdet till 0
+            
+            if(!countT[t[i]]) countT[t[i]]=0
+            countS[s[i]]++ // . a är första bokstaven, värdet på countS går från 0 till 1, index i av sträng s går från 0 till 1. 
+            //n är nästa bokstav värdet på countS går från 0 till 1 eftersom vi har inte sätt bokstaven n ännu. index i av sträng a går från 0 till 1.
+            countT[t[i]]++
+            console.log(t[i])
+        }
+        for(let ch in countS){
+            if(countS[ch] !== countT[ch]) return false
+        }
+        return true
     }
-    else{
-        return false;
-    }
-};
